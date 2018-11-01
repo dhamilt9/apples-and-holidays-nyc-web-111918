100eb6ad52189973741d11a9fccd9ec717f518f6
Daniel Hamilton <danhamilton16@gmail.com>
2018-11-01 21:36:15 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -65,7 +65,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, value|
     puts season
     value.each do |holiday, values|
-      puts holiday
+      puts holiday.to_s
     end
   end
   return holiday_hash

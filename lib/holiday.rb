0418d03a2b176f421b00c757cbe232346a2da55e
Daniel Hamilton <danhamilton16@gmail.com>
2018-11-01 21:33:06 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -63,7 +63,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |season, value|
-    puts key
+    puts season
     value.each do |holiday, values|
       puts holiday
     end
